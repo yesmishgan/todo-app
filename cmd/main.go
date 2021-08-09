437c80f3bfa39dbe119d08d4af042b88ffafd5be
Misha Dolbnin <dolbnin.mihail@yandex.ru>
2021-08-09 14:08:07 +0300

    Add app's architecture

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/yesmishgan/todo-app"
+	"github.com/yesmishgan/todo-app/pkg/handler"
+	"log"
+)
+
+func main(){
+	handlers := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
