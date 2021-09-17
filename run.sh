1877f4b7e6a2ff568f27cee13ce89a768e5cdb44
Misha Dolbnin <dolbnin.mihail@yandex.ru>
2021-09-17 15:03:50 +0300

    Add func to work with items in lists

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 docker run --name=todo_db -e POSTGRES_PASSWORD='qwerty' -p 5436:5432 -d --rm postgres
 docker ps
 docker exec -it <id>
+
+migrate -path ./todo-app/schema -database 'postgres://postgres:qwerty@localhost:5436/postgres?sslmode=disable' up
